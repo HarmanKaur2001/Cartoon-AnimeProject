ce50da79b8b6c33dff9c651cb8e6efcbc20f50f8
Harman Kaur <200456178@student.georgianc.on.ca>
2021-12-01 18:04:56 -0500

    changes in module-info.java and add dependencies in pom.xml e

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,14 @@ module com.example.cartoonanimeproject {
     requires javafx.fxml;
     requires javafx.graphics;
 
+    requires com.google.gson;
+    requires java.net.http;
+
+
+
 
     opens com.example.cartoonanimeproject to javafx.fxml;
     exports com.example.cartoonanimeproject;
+    opens com.example.cartoonanimeproject.Controllers to com.google.gson, javafx.fxml;
+
 }
